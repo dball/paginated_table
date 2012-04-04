1b5eca6b88f9631581d8a52ee1b0b549f90a4750
Donald Ball <donald.ball@gmail.com>
2012-04-04 17:23:17 -0400

    Refactor: Reorganizes specifications

--- test/integration/paginated_table_integration_test.rb
+++ test/integration/paginated_table_integration_test.rb
@@ -2,32 +2,37 @@ require 'test_helper'
 
 describe "paginated_table integration" do
 
-  before do
-    visit "/data"
-  end
+  describe "rendering" do
+    before do
+      visit "/data"
+    end
 
-  it "renders a paginated table" do
-    page.has_xpath?(table_xpath).must_equal true
-  end
+    it "renders a paginated table" do
+      page.has_xpath?(table_xpath).must_equal true
+    end
 
-  it "renders the first page of results" do
-    page.has_xpath?(tr_xpath(10)).must_equal true
-    page.has_xpath?(tr_xpath(11)).must_equal false
-  end
+    it "renders Name in the first column header" do
+      page.has_xpath?("#{th_xpath(1)}[.='Name']").must_equal true
+    end
 
-  it "renders Name in the first column header" do
-    page.has_xpath?("#{th_xpath(1)}[.='Name']").must_equal true
-  end
+    it "renders the data names in the first column" do
+      (1..10).each do |row|
+        page.has_xpath?("#{tr_xpath(row)}/td[1][.='Name #{row}']").must_equal true
+      end
+    end
 
-  it "renders the data names in the first column" do
-    (1..10).each do |row|
-      page.has_xpath?("#{tr_xpath(row)}/td[1][.='Name #{row}']").must_equal true
+    it "renders links to the data in the second column" do
+      (1..10).each do |row|
+        page.has_xpath?("#{tr_xpath(row)}/td[2]/a[@href='/data/#{row}'][.='#{row}']").must_equal true
+      end
     end
   end
 
-  it "renders links to the data in the second column" do
-    (1..10).each do |row|
-      page.has_xpath?("#{tr_xpath(row)}/td[2]/a[@href='/data/#{row}'][.='#{row}']").must_equal true
+  describe "pagination" do
+    it "renders one page of results" do
+      visit "/data"
+      page.has_xpath?(tr_xpath(10)).must_equal true
+      page.has_xpath?(tr_xpath(11)).must_equal false
     end
   end
 
@@ -46,5 +51,4 @@ describe "paginated_table integration" do
   def tr_xpath(row)
     "#{tbody_xpath}/tr[#{row}]"
   end
-
 end
