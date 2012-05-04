3a29c11699b9b595c245dcffdb95531a6dcbd34c
Donald Ball <donald.ball@gmail.com>
2012-05-04 10:31:58 -0400

    Refactors test data, adds search feature

--- test/dummy/app/controllers/data_controller.rb
+++ test/dummy/app/controllers/data_controller.rb
@@ -1,27 +1,40 @@
 require 'will_paginate/array'
 
 class DataController < ApplicationController
-  Datum = Struct.new(:id, :name)
-
-  DATA = (1..100).map do |i|
-    Datum.new(i, "Name #{i}")
-  end.shuffle
-
-  def DATA.order(arg)
-    column, direction = arg.split
-    sorted_data = case column
-    when 'name' then DATA.sort_by { |datum| datum[1] }
-    when 'id' then DATA.sort_by { |datum| datum[0] }
-    else raise "Invalid column: #{column}"
-    end
-    case direction
-    when 'asc' then sorted_data
-    when 'desc' then sorted_data.reverse
-    else raise "Invalid direction: #{direction}"
-    end
-  end
 
   def index
-    paginated_table('data', DATA, :defaults => { :sort_column => 'id' })
+    range =
+      case params['search']
+      when 'secondhalf' then (51..100)
+      else (1..100)
+      end
+    paginated_table('data', data(range), :defaults => { :sort_column => 'id' })
+  end
+
+  private
+
+  Datum = Struct.new(:id, :name)
+
+  module OrderableData
+    def order(arg)
+      column, direction = arg.split
+      sorted_data = case column
+      when 'name' then sort_by { |datum| datum[1] }
+      when 'id' then sort_by { |datum| datum[0] }
+      else raise "Invalid column: #{column}"
+      end
+      case direction
+      when 'asc' then sorted_data
+      when 'desc' then sorted_data.reverse
+      else raise "Invalid direction: #{direction}"
+      end
+    end
+  end
+    
+  def data(ids)
+    data = ids.map { |i| Datum.new(i, "Name #{i}") }
+    data.shuffle!
+    data.extend(OrderableData)
+    data
   end
 end
