fcff005e43a39d4b48c8a34cd54f314add6c3526
Donald Ball <donald.ball@gmail.com>
2012-07-17 21:05:26 -0400

    Refactors paginated_table method to accept varargs

--- lib/paginated_table/controller_helpers.rb
+++ lib/paginated_table/controller_helpers.rb
@@ -1,14 +1,27 @@
 module PaginatedTable
   module ControllerHelpers
-    def paginated_table(name, collection, options = {})
+    def paginated_table(*args)
+      options = args.extract_options!
+      case args.length
+      when 1
+        collection = args.first
+      when 2
+        name = args.first
+        collection = args.last
+      else
+        raise ArgumentError
+      end
       defaults = options.fetch(:defaults, {})
       page = PageParams.create_page(params, defaults)
       data_page = DataPage.new(collection, page)
-      instance_variable_set(:"@#{name}", data_page)
-      if request.xhr?
-        partial = options.fetch(:partial, name)
-        render :partial => partial, :layout => false
+      if name
+        instance_variable_set(:"@#{name}", data_page)
+        if request.xhr?
+          partial = options.fetch(:partial, name)
+          render :partial => partial, :layout => false
+        end
       end
+      data_page
     end
   end
 end
